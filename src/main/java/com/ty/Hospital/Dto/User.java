90096d18f194a9ba64b108216dc1041299e32f90
Manojmadegowda <manojmadegowda@gmail.com>
2022-06-16 17:06:59 +0530

    dto package

---
+++ src/main/java/com/ty/Hospital/Dto/User.java
@@ -0,0 +1,13 @@
+package com.ty.Hospital.Dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class User {
+	
+	
+
+}
