2361d69fc12572841334acd61b70f1e8f49ae899
Santhu4397 <santhoshdh129@gmail.com>
2022-06-23 13:05:46 +0530

    making some changes in pom.xml

--- src/main/java/com/ty/Hospital/Dto/User.java
+++ src/main/java/com/ty/Hospital/Dto/User.java
@@ -1,12 +1,33 @@
 package com.ty.Hospital.Dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collation ="Users")
 public class User {
+	@Id
+	private int id;
+	private String name;
+	private String emailId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 	
 	
 
