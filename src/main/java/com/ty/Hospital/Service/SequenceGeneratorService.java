9aa4f530e89aa869d13047c9e57266dcaa241bb2
Manojmadegowda <manojmadegowda@gmail.com>
2022-06-25 14:32:03 +0530

    no changes

--- src/main/java/com/ty/Hospital/Service/SequenceGeneratorService.java
+++ src/main/java/com/ty/Hospital/Service/SequenceGeneratorService.java
@@ -1,17 +1,17 @@
 package com.ty.Hospital.Service;
 
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
-
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import com.ty.Hospital.Dto.DataBase_Sequens;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
-import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
 
 @Service
 public class SequenceGeneratorService {
