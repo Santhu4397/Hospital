643b543652dc9d9718e45a22e4e5c2feb8ba2a88
Santhu4397 <santhoshdh129@gmail.com>
2022-06-27 12:26:14 +0530

    doing some changes in dtos

--- src/main/java/com/ty/Hospital/Dto/Branch.java
+++ src/main/java/com/ty/Hospital/Dto/Branch.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "Buildings")
+@Document(collation = "Branch")
 public class Branch {
 	@Transient
 	public static final String SEQUENCE_NAME = "branch_sequence";
--- src/main/java/com/ty/Hospital/Dto/Branch_Sequences.java
+++ src/main/java/com/ty/Hospital/Dto/Branch_Sequences.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
 
-@Document(collection = "hospital_Sequences")
+@Document(collection = "branch_Sequences")
 @Component
 public class Branch_Sequences {
 
