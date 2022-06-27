a872acc3425b2b40c80c0219998a8f93fd85675a
Polaka-Yerriswamy <polakayerriswamy1234@gmail.com>
2022-06-27 16:38:53 +0530

    swagger for building  controller

--- src/main/java/com/ty/Hospital/HospitalApplication.java
+++ src/main/java/com/ty/Hospital/HospitalApplication.java
@@ -2,8 +2,10 @@ package com.ty.Hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ty.Hospital")
 public class HospitalApplication {
 
 	public static void main(String[] args) {
