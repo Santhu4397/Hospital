77e12d573b477398237b81e9a16729b6b2daa26b
Santhu4397 <santhoshdh129@gmail.com>
2022-06-27 12:51:01 +0530

    adding hospital controller

--- src/main/java/com/ty/Hospital/Controller/HospitalController.java
+++ src/main/java/com/ty/Hospital/Controller/HospitalController.java
@@ -1,10 +1,14 @@
 package com.ty.Hospital.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,14 +26,26 @@ public class HospitalController {
 	@Autowired
 	SequenceGeneratorService service;
 	
-	@PostMapping("hospital/user/{uid}")
-	public ResponseEntity<ResponseStructure<Hospital>> saveHospital(@RequestBody Hospital hospital,@PathVariable int uid) {
+	@PostMapping("hospital/admin/{aid}")
+	public ResponseEntity<ResponseStructure<Hospital>> saveHospital(@RequestBody Hospital hospital,@PathVariable int aid) {
 		hospital.setId(service.generateHospitalSequence(Hospital.SEQUENCE_NAME));
 		return null;
 	}
 	@GetMapping("hospital/hid/{id}")
 	public ResponseEntity<ResponseStructure<Hospital>> getById(@RequestParam int id){
 		return null;
-		
 	}
+	@GetMapping("hospital")
+	public ResponseEntity<ResponseStructure<List<Hospital>>> getall(){
+		return null;
+	}
+	@PutMapping("hospital/{hid}/admin/{aid}")
+	public ResponseEntity<ResponseStructure<Hospital>> update(@RequestBody Hospital hospital, @PathVariable int hid ,@PathVariable int aid){
+		return null;
+	}
+	@DeleteMapping("hospital")
+	public ResponseEntity<ResponseStructure<Boolean>> delete(@RequestParam int id){
+		return null;
+	}
+	
 }
