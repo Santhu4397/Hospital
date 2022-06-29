bf43f1324b9f0636da1b138a22e74e5cde87d357
Polaka-Yerriswamy <polakayerriswamy1234@gmail.com>
2022-06-29 11:48:14 +0530

    UserDao impl

---
+++ src/main/java/com/ty/Hospital/daoimpl/UserDaoImpl.java
@@ -0,0 +1,55 @@
+package com.ty.Hospital.daoimpl;
+
+import java.rmi.server.UID;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.Hospital.Dto.User;
+import com.ty.Hospital.Repo.UserRepo;
+import com.ty.Hospital.dao.UserDao;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public User saveUSer(User user) {
+		return userRepo.save(user);
+	}
+
+	@Override
+	public User getUserById(int uid) {
+		Optional<User> optional = userRepo.findById(uid);
+		if (optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+
+	@Override
+	public List<User> getAllUser() {
+		return userRepo.findAll();
+	}
+
+	@Override
+	public User updateUserById(int uid, User user) {
+		User existingUser = getUserById(uid);
+		if(existingUser != null) {
+			existingUser.setName(user.getName());
+			existingUser.setAge(user.getAge());
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteUser(int uid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
