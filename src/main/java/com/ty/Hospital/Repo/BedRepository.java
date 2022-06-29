483f1ae5bfddef48fa658a7440fdcf5b653d3e82
Polaka-Yerriswamy <polakayerriswamy1234@gmail.com>
2022-06-29 15:22:07 +0530

    modify HospitalDao impl

--- src/main/java/com/ty/Hospital/Repo/BedRepository.java
+++ src/main/java/com/ty/Hospital/Repo/BedRepository.java
@@ -1,7 +1,5 @@
 package com.ty.Hospital.Repo;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -9,7 +7,7 @@ import com.ty.Hospital.Dto.Bed;
 
 public interface BedRepository extends MongoRepository<Bed, Integer>{
 	
-	@Query("{id :?0}")                                                  //SQL Equivalent : SELECT * FROM Bed WHERE ID=?
-    public Bed getBedById(Integer id);
+	@Query("{'id' :?0}")                                                  //SQL Equivalent : SELECT * FROM Bed WHERE ID=?
+    public Bed getBedById(int id);
 
 }
